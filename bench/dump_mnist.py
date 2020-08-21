15dfbb428fc9eccdd53e26438f38a18b26b7bf28
fzzle <e@weihe.dk>
2020-08-21 12:26:28 +0200

    Dump mnist as Futhark data

---
+++ bench/dump_mnist.py
@@ -0,0 +1,9 @@
+from idx2numpy import convert_from_file
+from futhark_svm import SVC
+
+X_train = convert_from_file('./data/train-images-idx3-ubyte')
+y_train = convert_from_file('./data/train-labels-idx1-ubyte')
+X_train = X_train.reshape(60000, 784)
+
+m = SVC(kernel='polynomial')
+m.dump_fit(X_train, y_train, 'poly_mnist.data')
